31799a6cf6953804aa728c4f8ef29a1d4c94bd70
Robert Gregg <hi@robertgregg.co.uk>
2021-01-30 21:48:04 +0000

    Added counters to map

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -1,11 +1,41 @@
-import { GoogleMap } from "react-google-maps";
+import React, { useEffect } from "react";
+import { GoogleMap, Marker } from "react-google-maps";
+import CounterLocations from "../trafficCounterLocationData.json";
 
 function Map() {
+  useEffect(() => {
+    console.log(
+      CounterLocations.d2LogicalModel.payloadPublication.measurementSiteTable
+        .measurementSiteRecord[1].measurementSiteLocation.tpeglinearLocation.to
+        .pointCoordinates.longitude
+    );
+  }, []);
+
   return (
-    <GoogleMap
-      defaultZoom={10}
-      defaultCenter={{ lat: 55.854434529999814, lng: -4.216974959509279 }}
-    />
+    <div>
+      <GoogleMap
+        defaultZoom={15}
+        defaultCenter={{ lat: 55.854434529999814, lng: -4.216974959509279 }}
+      />
+
+      {CounterLocations.d2LogicalModel.payloadPublication.measurementSiteTable.measurementSiteRecord.map(
+        (counter) => (
+          <Marker
+            key={counter["@id"]}
+            position={{
+              lat: parseFloat(
+                counter.measurementSiteLocation.tpeglinearLocation.to
+                  .pointCoordinates.latitude
+              ),
+              lng: parseFloat(
+                counter.measurementSiteLocation.tpeglinearLocation.to
+                  .pointCoordinates.longitude
+              ),
+            }}
+          />
+        )
+      )}
+    </div>
   );
 }
 
