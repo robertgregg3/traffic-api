4a400996d8aefec080b450d7de1b40a09ca400ee
Robert Gregg <hi@robertgregg.co.uk>
2021-02-01 10:30:50 +0000

    Added table skeleton for collated data

--- src/Components/CollatedData.js
+++ src/Components/CollatedData.js
@@ -11,7 +11,7 @@ const locationData =
 const movementData =
   MovementDataJson.d2LogicalModel.payloadPublication.siteMeasurements;
 
-function CollatedData() {
+function CollatedData({ sortColumn, onSort }) {
   const [collatedData, setCollatedData] = useState([]);
 
   useEffect(() => {
@@ -41,79 +41,66 @@ function CollatedData() {
   }, []);
 
   return (
-    <div className="data">
-      {collatedData &&
-        collatedData.map((data) => (
-          <ul key={data.id} className="data__container">
-            <li>
-              <h4>ID: </h4>
-              {data.id}
-            </li>
-            <li>
-              <h4>Measurement Site Reference:</h4>
-              {data.measurementSiteReference}
-            </li>
-            <li>
-              <h4>(From) Latitude: </h4>
-              {
-                data.measurementSiteLocation.tpeglinearLocation.to
-                  .pointCoordinates.latitude
-              }
-            </li>
-            <li>
-              <h4>(From) Longitude: </h4>
-              {
-                data.measurementSiteLocation.tpeglinearLocation.to
-                  .pointCoordinates.longitude
-              }
-            </li>
-            <li>
-              <h4>From: </h4>
-              {
-                data.measurementSiteLocation.tpeglinearLocation.to.name
-                  .descriptor.value
-              }
-            </li>
-            <li>
-              <h4>(To) Latitude: </h4>
-              {
-                data.measurementSiteLocation.tpeglinearLocation.from
-                  .pointCoordinates.latitude
-              }
-            </li>
-            <li>
-              <h4>(To) Longitude: </h4>
-              {
-                data.measurementSiteLocation.tpeglinearLocation.from
-                  .pointCoordinates.longitude
-              }
-            </li>
-            <li>
-              <h4>To: </h4>
+    <table className="table">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Traffic Flow</th>
+          <th>Traffic Concentration</th>
+          <th>(From) Description</th>
+          <th>(To) Description</th>
+          <th>(From) Lat</th>
+          <th>(To) Lat</th>
+          <th>(From) Long</th>
+          <th>(To) Long</th>
+        </tr>
+      </thead>
+      <tbody>
+        {collatedData.map((data) => (
+          <tr key={data.id}>
+            <td>{data.id}</td>
+            <td>{data.measuredValue[0].basicDataValue.vehicleFlow}</td>
+            <td>{data.measuredValue[1].basicDataValue.concentration}</td>
+            <td>
               {
                 data.measurementSiteLocation.tpeglinearLocation.from.name
                   .descriptor.value
               }
-            </li>
-            <li>
-              <h4>Measurement Time Default:</h4>
-              {data.measurementTimeDefault}
-            </li>
-            <li>
-              <h4>Traffic Flow: </h4>
-              {!data.measuredValue
-                ? ""
-                : data.measuredValue[0].basicDataValue.vehicleFlow}
-            </li>
-            <li>
-              <h4>Traffic Concentration: </h4>
-              {!data.measuredValue
-                ? ""
-                : data.measuredValue[1].basicDataValue.concentration}
-            </li>
-          </ul>
+            </td>
+            <td>
+              {
+                data.measurementSiteLocation.tpeglinearLocation.to.name
+                  .descriptor.value
+              }
+            </td>
+            <td>
+              {
+                data.measurementSiteLocation.tpeglinearLocation.from
+                  .pointCoordinates.latitude
+              }
+            </td>
+            <td>
+              {
+                data.measurementSiteLocation.tpeglinearLocation.to
+                  .pointCoordinates.latitude
+              }
+            </td>
+            <td>
+              {
+                data.measurementSiteLocation.tpeglinearLocation.from
+                  .pointCoordinates.longitude
+              }
+            </td>
+            <td>
+              {
+                data.measurementSiteLocation.tpeglinearLocation.to
+                  .pointCoordinates.longitude
+              }
+            </td>
+          </tr>
         ))}
-    </div>
+      </tbody>
+    </table>
   );
 }
 
